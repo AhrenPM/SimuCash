bf2500470658912a0be6d2aaf75fdb5dc251e45c
shaohuakdi@gmail.com <shaohuakdi@gmail.com>
2022-04-10 12:30:03 -0400

    overflow resolution

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -33,6 +33,18 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    // Debug purpose
+    /*for (var i = 0; i<=20; i++){
+      widget.ownerCard.addTransaction(Transaction(
+          transfer_time: DateTime.now(),
+          origin_card_key: '00000',
+          amount: receivedAmount(),
+          transfer_confirm: true,
+          destination_card_key: widget.ownerCard.card_key,
+          transfer_id: generateTransferID('00000', widget.ownerCard.card_key, DateTime.now())
+      ));
+    }*/
+    //
     return Scaffold(
         appBar: AppBar(
           title: Image.asset(
@@ -52,20 +64,21 @@ class _HomePageState extends State<HomePage> {
             ),
           ],
         ),
-        body: Center(
+        body: Align(
+          alignment: Alignment.topCenter,
           child: Column(children: [
             Padding(
               padding: const EdgeInsets.only(top: 30),
               child: Card(
                 child: Padding(
                   padding: const EdgeInsets.symmetric(
-                      horizontal: 80.0, vertical: 20),
+                      horizontal: 80.0, vertical: 14),
                   child: Column(children: [
                     const Text('Account Balance',
                         style: TextStyle(
                             fontSize: 24, fontWeight: FontWeight.bold)),
                     Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 20.0),
+                        padding: const EdgeInsets.symmetric(vertical: 14.0),
                         child: Text('\$'+double.parse(widget.ownerCard.amount.toStringAsFixed(2)).toString(),
                             style: const TextStyle(
                                 color: Color(0xffbe9e44),
@@ -75,51 +88,82 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 0),
-              child: widget.ownerCard.numTransaction()>=0
-                  ? DataTable(
-                  columns: const <DataColumn>[
-                    DataColumn(
-                      label: Text(
-                        'Date',
-                        style: TextStyle(fontStyle: FontStyle.italic),
-                      ),
-                    ),
-                    DataColumn(
-                      label: Text(
-                        'Type',
-                        style: TextStyle(fontStyle: FontStyle.italic),
-                      ),
-                    ),
-                    DataColumn(
-                      label: Text(
-                        'Amount',
-                        style: TextStyle(fontStyle: FontStyle.italic),
-                      ),
-                    ),
-                  ],
-                  rows: List<DataRow>.generate(
-                    widget.ownerCard.numTransaction()+1,
-                        (int index) => DataRow(
-                      cells: <DataCell>[
-                        DataCell(Text(widget.ownerCard.transactions[index].transfer_time.year.toString()+"-"+widget.ownerCard.transactions[index].transfer_time.month.toString()+"-"+widget.ownerCard.transactions[index].transfer_time.day.toString())),
-                        DataCell(Text(widget.ownerCard.transactions[index].amount>0
-                            ? "Received"
-                            : "Sent"
-                        )),
-                        DataCell(Text('\$'+widget.ownerCard.transactions[index].amount.abs().toString())),
-                      ],
-                    ),
-                  )
-              )
-                  : const Center(
-                  child: Text('No Transactions Yet',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600),
-                  )),
+            Expanded(
+              child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 0),
+                    child: widget.ownerCard.numTransaction()>=0
+                        ?Wrap(
+                        children: <Widget>[
+                          DataTable(
+                              columns: const <DataColumn>[
+                                DataColumn(
+                                  label: Text(
+                                    'Date',
+                                    style: TextStyle(fontStyle: FontStyle.italic),
+                                  ),
+                                ),
+                                DataColumn(
+                                  label: Text(
+                                    'Type',
+                                    style: TextStyle(fontStyle: FontStyle.italic),
+                                  ),
+                                ),
+                                DataColumn(
+                                  label: Text(
+                                    'Amount',
+                                    style: TextStyle(fontStyle: FontStyle.italic),
+                                  ),
+                                ),
+                              ],
+                              rows: List<DataRow>.generate(
+                                widget.ownerCard.numTransaction()+1,
+                                    (int index) => DataRow(
+                                  cells: <DataCell>[
+                                    DataCell(Text(widget.ownerCard.transactions[index].transfer_time.year.toString()+"-"+widget.ownerCard.transactions[index].transfer_time.month.toString()+"-"+widget.ownerCard.transactions[index].transfer_time.day.toString())),
+                                    DataCell(Text(widget.ownerCard.transactions[index].amount>0
+                                        ? "Received"
+                                        : "Sent"
+                                    )),
+                                    DataCell(Text('\$'+widget.ownerCard.transactions[index].amount.abs().toString())),
+                                  ],
+                                ),
+                              )
+                          )
+                        ]
+                    )
+                        : DataTable(
+                        columns: const <DataColumn>[
+                          DataColumn(
+                            label: Text(
+                              'Date',
+                              style: TextStyle(fontStyle: FontStyle.italic),
+                            ),
+                          ),
+                          DataColumn(
+                            label: Text(
+                              'Type',
+                              style: TextStyle(fontStyle: FontStyle.italic),
+                            ),
+                          ),
+                          DataColumn(
+                            label: Text(
+                              'Amount',
+                              style: TextStyle(fontStyle: FontStyle.italic),
+                            ),
+                          ),
+                        ], rows: const [
+                          DataRow(
+                              cells: [
+                                DataCell(Text('1111-11-11', style: TextStyle(color: Colors.white,),)),
+                                DataCell(Text('received', style: TextStyle(color: Colors.white,),)),
+                                DataCell(Text('\$0000.00', style: TextStyle(color: Colors.white,),)),
+                    ]),
+                    ]
+                    )
+                  ),
+              ),
             ),
           ]),
         ),
@@ -195,7 +239,8 @@ class _HomePageState extends State<HomePage> {
               ),
             ],
           ),
-        ));
+        )
+    );
   }
 
   void _asyncTransferPage(BuildContext context) async {
